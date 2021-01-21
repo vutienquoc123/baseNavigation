b86a3457bf10a2b479d85b744a5bde86fe9336b8
vutienquoc123 <50628209+vutienquoc123@users.noreply.github.com>
2021-01-21 11:14:32 +0700

    theme

---
+++ src/themes/index.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+const GlobalStyle = StyleSheet.create({
+  fullScreen: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+});
+export {GlobalStyle};
+export * from './color';
+export * from './spacing';
+export * from './timing';
+
